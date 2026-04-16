01a49cdb39a8fbd18d76b2ed13a7086213d9c605
Akshat Chitransh <akshat.chitransh3108@gmail.com>
2026-04-16 16:00:54 +0530

    check

--- src/index.ts
+++ src/index.ts
@@ -28,7 +28,7 @@ app.get("/webhook", (req, res) => {
 
   console.log("FULL BOY:", JSON.stringify(data, null, 2));
 
-  res.sendStatus(200).json({webhook:"ye wala route woeking"});
+  res.sendStatus(200).send({webhook:"ye wala route woeking"});
 });
 
 const PORT = process.env.PORT;

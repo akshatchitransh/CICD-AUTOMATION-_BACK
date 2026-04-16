ddb756594f72049b92e1ea2f3abb49850dbe7294
Akshat Chitransh <akshat.chitransh3108@gmail.com>
2026-04-16 15:07:20 +0530

    testing webhook

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectdb } from "./utils/db.js";
+import cors from "cors"
+
+
 
 dotenv.config();
 const app = express();
+
+app.use(cors())
 app.use(express.json());
 connectdb();
 
@@ -15,6 +20,16 @@ app.get('/show',(req:any,res:any)=>{
 app.post('/analyse',(req:any,res:any)=>{
    res.send({ message: "analysing" })
 })
+
+app.post("/webhook", (req, res) => {
+  console.log("🔥 Webhook Triggered!");
+
+  const data = req.body;
+
+  console.log("FULL BODY:", JSON.stringify(data, null, 2));
+
+  res.sendStatus(200);
+});
 
 const PORT = process.env.PORT;
 app.listen(PORT,()=>{
